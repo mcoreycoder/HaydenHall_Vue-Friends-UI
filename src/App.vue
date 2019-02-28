07887aea2308a0fcda46e24cc29692d8718a49ab
Kealii <kleechipsao@gmail.com>
2019-02-28 10:57:21 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav>
+      <div class='container'>
+        <ul class='nav__left'>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/admin">Admin</router-link></li>
+          <li><router-link to="/cart">Cart</router-link></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>
+      <div class="container">
+          <ul>
+            <!-- Links are to sibling routes -->
+            <li><router-link to="/admin">View Products</router-link></li>
+            <li><router-link to="/admin/new">New Product</router-link></li>
+          </ul>
+        <!-- Outlet for children routes -->
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
