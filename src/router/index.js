41f2c4d02c3df31e0a6f836b0801591fddb3f357
Michael Corey <fishincorey@gmail.com>
2019-03-24 08:35:42 -0600

    crud dialed in

--- src/router/index.js
+++ src/router/index.js
@@ -1,103 +1,143 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-// import VeeValidate from 'vee-validate'
+// import Router from 'vue-router'
 
+// Vue.use(Router)
 
-Vue.use(Router)
+const apiURL = 'http://localhost:3000'
 
-const apiUrl = "http://localhost:3000/friends"
 const app = new Vue({
   el: '#app',
   data: {
-    delFriend: null,
-    makeFriend: null,
-    editFriend: null,
-    idFriend: null,
-    friends: []
+    id: null,
+    inputAge: null,
+    inputName: null,
+    friends: [],
+    friend: null
   },
   methods: {
-    deleteFriend(event) {
-        fetch(`${apiUrl}/_id`, {
-            method: "DELETE"
-        })
-        .then(() => {
-            this.getFriends()
-        })
-        .then(() => {
-            console.log("Deleted")
-        })
-    },
-
+    // below is working
     getFriends() {
-        fetch(apiUrl)
+      fetch(`${apiURL}/friends`)
         .then(response => {
-            return response.json();
+          return response.json()
         })
         .then((data) => {
-            this.friends = data;
+          this.friends = data
         })
     },
-    updateFriend(event) {
-        fetch(apiUrl, {
-            body: JSON.stringify(event),
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then(() => {
-                this.editFriend = null;
-            })
-    },
-    createFriend(event) {
-        fetch(apiUrl, {
-            body: JSON.stringify({name : this.editFriend, age : this.makeFriend}),
-            method: "POST",
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-        })
-            .then(() => {
-                this.editFriend = null;
-            })
-            .then(() => {
-                this.makeFriend = null;
-            })
-            .then(() => {
-                this.getFriends()
-            })
-            .then(() => {
-                console.log("Created!!", event)
-            })
-    },
-},
-mounted() {
-    this.getFriends()
-},
+    // working on below... might be best to have in another component
+    // getFriend (_id) {
+    //   fetch(`${apiURL}/friend/${_id}`)
+    //     .then(response => {
+    //       return response.json()
+    //     })
+    //     .then((data) => {
+    //       this.friend = data
+    //     })
+    // },
 
-template: `
+    // below is working
+    createFriend(friend) {
+      console.log('createFriend:', { name: this.inputName, age: this.inputAge })
+      // this.id = this.friends.length + 1
+      fetch(`${apiURL}/friend`, {
+        body: JSON.stringify({ name: this.inputName, age: this.inputAge }),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res =>
+          console.log('res', res))
+        .then(() => {
+          this.inputName = null
+          this.inputAge = null
+          this.getFriends()
+        })
+        .then(() =>
+          console.log('Created!!', friend))
+    },
+
+    // below is working
+    updateFriend(friend) {
+      console.log('updateFriend:', friend)
+      fetch(`${apiURL}/friend/${friend._id}`, {
+        body: JSON.stringify(friend),
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(() => {
+          this.id = null
+        })
+    },
+
+    // below is working
+    deleteFriend(friend) {
+      console.log('deleteFriend:', friend)
+      fetch(`${apiURL}/friend/${friend._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(res =>
+          console.log(res))
+        .then(() => {
+          this.getFriends()
+        })
+        .catch(err =>
+          console.log(err))
+    },
+
+    cancel(friend) {
+      this.id = null
+      this.inputName = null
+      this.inputAge = null
+      this.getFriends()
+    }
+  },
+
+  mounted() {
+    this.getFriends()
+
+  },
+
+  template: `
 <div>
     <ul>
         <li>
             <div>
-                <li>Name: <input v-model="editFriend"></input></li>
-                <li>Age: <input v-model="makeFriend"></input></li>
-                <button v-on:click="createFriend">Create Friend</button>
+                <li>Name: <input v-model="inputName"></input></li>
+                <li>Age: <input v-model="inputAge"></input></li>
+                <button v-on:click="createFriend(friend)">Create Friend</button>
             </div>
         </li>
+        <hr>
+        <hr>
         <li v-for= "friend, i in friends">
-            <div v-if="editFriend === friend.id">
-                <input v-model="friend.name" />
+            <div v-if="id === friend._id">
+            <hr>
+            <li>ID: {{friend._id}}</li>
+            <li>Name: <input v-model="friend.name" /> </li>
+            <li>Age: <input v-model="friend.age" /> </li>
                 <button v-on:click="updateFriend(friend)">save</button>
+                <button v-on:click="cancel(friend)">cancel</button>
+                <hr>
+                <hr>
             </div>
             <div v-else>
+            <li>ID: {{friend._id}}</li>
             <li>Name: {{friend.name}}</li>
             <li>Age: {{friend.age}}</li>
-                <button v-on:click="editFriend = friend.id">Edit</button>
-                <button v-on:click="deleteFriend" name= {{friend._id}}>X</button>
+                <button v-on:click="id = friend._id">Edit</button>
+                <button v-on:click="deleteFriend(friend)">X</button>
+                <hr>
             </div>
         </li>
     </ul>
 </div>
 `
-})
+}
+)
